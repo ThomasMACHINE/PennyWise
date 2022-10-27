50ce1142813fc4f24a03986805e39393f168c440
ThomasMACHINE <thomasvl@stud.ntnu.no>
2022-10-27 08:54:25 +0100

    Implemented HiveMind for all characters

---
+++ Assets/Scripts/Character/NPC/NpcHiveMind.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcHiveMind : MonoBehaviour
+{
+    [SerializeField] public List<AggressiveCharacter> walkingCharacters = new List<AggressiveCharacter>();
+    [SerializeField] public List<AggressiveCharacter> aggressiveCharacters = new List<AggressiveCharacter>();
+    
+    [SerializeField] private float moveCooldown = 1;
+    [SerializeField] private float searchCooldown = 1;
+
+    private float moveTimer = 0;
+    private float searchTimer = 0;
+
+    // If you are lazy, (In a good way) implement this start function
+    [SerializeField] private bool iAmLazy = false;
+    void Start()
+    {
+        if (iAmLazy) 
+        { 
+            //Loop through all gameobjects in the scene and find Charachters
+        }
+    }
+
+
+    void Update()
+    {
+        moveTimer += Time.deltaTime;
+        searchTimer += Time.deltaTime;
+
+        if (moveTimer > moveCooldown)
+        {
+            moveTimer = 0;
+            foreach (IWalkingCharacter character in walkingCharacters){
+                character.DoMove();
+            }
+        }
+
+        if (searchTimer > searchCooldown)
+        {
+            searchTimer = 0;
+            foreach (IAggressiveEnemy character in aggressiveCharacters) 
+            {
+                character.SearchForPlayer();
+            }
+        }
+    }
+}
